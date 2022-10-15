5203d82b9e850e687ddfe2dd3ab390726edd3b1f
Aisha Yusuffe <a.yusuff99@outlook.com>
2022-10-15 23:10:28 +0100

    pass first test to return vowel count

---
+++ vowelCounter.js
@@ -0,0 +1,14 @@
+// Using a TDD approach, write code to determine the total number of vowels in a phrase.
+// Example:
+// “I am a Java engineer”
+// A – 4
+// E - 3
+// I - 2
+// O - 0
+// U – 0
+
+const vowelCounter = (string) => {
+  return "A - 0\nE - 0\nI - 0\nO - 0\nU - 0";
+};
+
+module.exports = vowelCounter;
